d226237359b2b5f67fa56dc1bb02851e9c9d4356
Bljek <BlackDrugsGaming@gmail.com>
2020-04-18 20:13:14 +0200

    Zadanie 0.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,5 +25,15 @@ public class Main {
         me.pet.feed();
 
         System.out.println(me.pet.species);
+
+        Animal cat = new Animal("cat");
+        cat.feed();
+
+        for (int i = 0; i < 52; i++)
+        {
+            cat.takeForAWalk();
+        }
+
+
     }
 }
