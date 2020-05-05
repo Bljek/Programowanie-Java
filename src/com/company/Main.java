8906f81d17c63f22fc34ef8f3d6724c1d574a994
Bljek <BlackDrugsGaming@gmail.com>
2020-05-05 17:22:31 +0200

    Zadanie 3 i 4, getter/setter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,32 +7,33 @@ public class Main {
         Animal dog = new Animal("Dog");
         dog.name = "Hacia";
 
-        System.out.println("Hi, I'm " + dog.name);
+        //System.out.println("Hi, I'm " + dog.name);
 
-        dog.feed();
+        //dog.feed();
 
         Human me = new Human();
         me.firstName = "Karol";
         me.lastName = "Glaza";
         me.pet = dog;
+        me.setSalary(2600.00);
 
-        me.pet.feed();
-        System.out.println(me.pet.species);
+        //me.pet.feed();
+        //System.out.println(me.pet.species);
 
-        me.pet = new Animal("Lion");
-        me.pet.name = "Myszoskoczek";
+        //me.pet = new Animal("Lion");
+       // me.pet.name = "Myszoskoczek";
 
-        me.pet.feed();
+        //me.pet.feed();
 
-        System.out.println(me.pet.species);
+        //System.out.println(me.pet.species);
 
-        Animal cat = new Animal("cat");
-        cat.feed();
+        //Animal cat = new Animal("cat");
+        //cat.feed();
 
-       // for (int i = 0; i < 52; i++)
-       // {
-       //     cat.takeForAWalk();
-       // }
+        //for (int i = 0; i < 52; i++)
+        //{
+        //   cat.takeForAWalk();
+        //}
 
         Car car = new Car("Astra", "Opel");
         car.engineSize = 1.5;
@@ -40,6 +41,13 @@ public class Main {
         car.kolor = "zielony";
 
         me.car = car;
+
+        me.setSalary(2600.0);
+        System.out.println(me.getSalary());
+        me.setSalary(3000.0);
+        System.out.println(me.getSalary());
+
+
 
 
     }
